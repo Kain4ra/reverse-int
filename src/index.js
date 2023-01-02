7dbee7a7a292e86f90713eba501c8f88ca3d9325
KainAra <kainara.idwsi@gmail.com>
2023-01-02 18:18:02 +0300

    ref: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  if (String(n)[0] == '-') {
+    for (let i = String(n).length - 1; i > 0; i--) {
+      result += String(n)[i];
+    }
+  }
+  else {
+    for (let i = String(n).length - 1; i >= 0; i--) {
+      result += String(n)[i];
+    }
+  }
+  return +result;
 }
